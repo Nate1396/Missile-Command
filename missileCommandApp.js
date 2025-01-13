7c552ed3c4dd119d62756730ba8d3519698d71b2
Nate1396 <68649081+Nate1396@users.noreply.github.com>
2025-01-13 14:21:01 -0500

    Add files via upload

---
+++ missileCommandApp.js
@@ -0,0 +1,104 @@
+const canvas = document.getElementById('game');
+const context = canvas.getContext('2d');
+
+const startingAmmo = 10;
+const groundY = 100;
+
+const DefenseMissileSpeed = 1;
+const AttackMissileSpeed = 15;
+
+let cities = [];
+let silos = [];
+let aMissiles = [];
+let dMissiles = [];
+let explosions = [];  
+
+class City {
+    constructor(ax, ay){
+        this.alive = true;
+        this.width = 50;
+        this.height = 50;
+        this.position = {x: ax, y: ay};
+        this.color = "blue";
+    }
+} 
+
+class Silo {
+    constructor(ix, iy){
+        this.alive = true;
+        this.position = {x:ix, y:iy};
+        this.angle = 90;
+        this.ammo = startingAmmo;
+        this.diameter = 100;
+    }
+}
+
+class AttackMissile {
+    constructor(ix, iy, fx, fy){
+        this.position = {x:ix, y:iy};
+        this.destination = {x:fx, y:fy};
+        this.vector = {dx:fx-ix, dy:fy-iy}
+    }
+}
+
+class DefenseMissile {
+    constructor(ix, iy, fx, fy){
+        this.position = {x:ix, y:iy};
+        this.destination = {x:fx, y:fy};
+        this.vector = {dx:fx-ix, dy:fy-iy}
+    }
+}
+
+class Explosion {
+    constructor(ix, iy){
+        this.center = {x:ix, y:iy};
+        this.radius = 0;}
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function angleBetweenPoints(source, target) {
+    return Math.atan2(target.y - source.y, target.x - source.x) + Math.PI / 2;
+  }
+  
+function distance(source, target) {
+    return Math.hypot(source.x - target.x, source.y - target.y);
+  }
+
+function render(){
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = "green";
+    context.fillRect(0,canvas.height-groundY,canvas.width,groundY);
+    context.fillStyle = "blue";
+    for (i = 0; i < cities.length;i++){
+        context.fillRect(cities[i].position.x,canvas.height-cities[i].position.y-cities[i].height,cities[i].width,cities[i].height)
+    }
+    for (i = 0; i < silos.length; i++){
+        context.beginPath();
+        context.arc(silos[i].position.x, canvas.height-silos[i].position.y, 50, Math.PI, 0);
+        context.fillStyle = "red";
+        context.fill();
+        context.stroke();
+
+    }
+   
+
+}
+
+for (i=0;i<3;i++){
+    cities.push(new City(225+i*100,groundY));
+
+}
+
+for (i=0;i<3;i++){
+    cities.push(new City(725+i*100,groundY));
+
+}
+
+for (i=0;i<3;i++){
+    silos.push(new Silo(112.5+487.5*i,groundY));
+}
+
+render();
