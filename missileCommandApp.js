5b606885a669f745ac1ab42bcf32f4aee6e48e9b
Nate1396 <68649081+Nate1396@users.noreply.github.com>
2025-01-17 13:10:30 -0500

    Update missileCommandApp.js

--- missileCommandApp.js
+++ missileCommandApp.js
@@ -21,50 +21,63 @@ class City {
         this.position = {x: ax, y: ay};
         this.color = "blue";
     }
+
+    drawCity(){
+        if (this.alive === true){
+            context.fillStyle = this.color;
+            context.fillRect(this.position.x, this.position.y, this.width, this.height);
+        }
+    }
 } 
 
 class Silo {
     constructor(ix, iy){
         this.alive = true;
         this.position = {x:ix, y:iy};
-        this.angle = Math.PI / 2; 
+        this.angle = 90;
         this.ammo = startingAmmo;
         this.diameter = 100;
-        this.radius = 50; 
     }
 
-    
-    drawTurret() {
-        const turretLength = 30;
-        const turretWidth = 10;
-        const turretX = this.position.x;
-        const turretY = canvas.height - this.position.y - this.radius - 18;
-
-        context.save();
-        context.translate(turretX, turretY);
-        context.rotate(this.angle);
-
-        
-        context.fillStyle = 'gray';
-        context.fillRect(0, -turretWidth / 2, turretLength, turretWidth);
-
-        context.restore();
+    drawSilo(){
+        if (this.alive === true){
+            context.beginPath();
+            context.arc(this.position.x, this.position.y, 50, Math.PI, 0);
+            context.fillStyle = "red";
+            context.fill();
+            context.stroke();
+        }
     }
 }
 
 class AttackMissile {
     constructor(ix, iy, fx, fy){
-        this.position = {x:ix, y:iy};
+        this.start = {x:ix, y:iy};
         this.destination = {x:fx, y:fy};
-        this.vector = {dx:fx-ix, dy:fy-iy};
-       
-        
+        this.position = {x:ix, y:iy};
+        this.xSide = (this.destination.x - this.start.x);
+        this.ySide = (this.destination.y - this.start.y);
+        this.dx = this.xSide/(Math.sqrt(this.xSide**2+this.ySide**2));
+        this.dy = this.ySide/(Math.sqrt(this.xSide**2+this.ySide**2));
+    }
+
+    drawMissile(){
+        context.lineWidth = 5;
+        context.strokeStyle = "red";
+        context.moveTo(this.start.x, this.start.y);
+        context.lineTo(this.position.x, this.position.y);
+        context.stroke();
+    }
+
+    move(){
+        this.position.x += this.dx;
+        this.position.y += this.dy;
     }
 }
 
 class DefenseMissile {
     constructor(ix, iy, fx, fy){
-        this.position = {x:ix, y:iy};
+        this.start = {x:ix, y:iy};
         this.destination = {x:fx, y:fy};
         this.vector = {dx:fx-ix, dy:fy-iy};
     }
@@ -73,7 +86,14 @@ class DefenseMissile {
 class Explosion {
     constructor(ix, iy){
         this.center = {x:ix, y:iy};
-        this.radius = 0;
+        this.radius = 0};
+
+    drawExplosion(){
+        context.beginPath();
+        context.arc(this.center.x, this.center.y, this.radius, Math.PI*2, 0);
+        context.fillStyle = "white";
+        context.fill();
+        context.stroke();
     }
 }
 
@@ -82,47 +102,85 @@ function randInt(min, max) {
 }
 
 function angleBetweenPoints(source, target) {
-    return Math.atan2(target.y - source.y, target.x - source.x);
-}
+    return Math.atan2(target.y - source.y, target.x - source.x) + Math.PI / 2;
+  }
   
 function distance(source, target) {
     return Math.hypot(source.x - target.x, source.y - target.y);
-}
+  }
 
 function render(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height)
     context.fillStyle = "green";
-    context.fillRect(0, canvas.height - groundY, canvas.width, groundY);
-    context.fillStyle = "blue";
-    for (let i = 0; i < cities.length; i++){
-        context.fillRect(cities[i].position.x, canvas.height - cities[i].position.y - cities[i].height, cities[i].width, cities[i].height);
+    context.fillRect(0,canvas.height-groundY,canvas.width,groundY);
+
+    for (i = 0; i < cities.length;i++){
+        cities[i].drawCity();
     }
-    for (let i = 0; i < silos.length; i++){
-        
-        context.beginPath();
-        context.arc(silos[i].position.x, canvas.height - silos[i].position.y, silos[i].radius, Math.PI, 0);
-        context.fillStyle = "red";
-        context.fill();
-        context.stroke();
+    
+    for (i = 0; i < silos.length; i++){
+        silos[i].drawSilo();
+    }
+   
+    for (i = 0; i < explosions.length; i++){
+        explosions[i].drawExplosion();
+    }    
 
-        
-        silos[i].drawTurret();
+    for (i = 0; i < aMissiles.length; i++){
+        aMissiles[i].drawMissile();
     }
 }
 
-for (let i = 0; i < 3; i++){
-    cities.push(new City(225 + i * 100, groundY));
+for (let i=0;i<3;i++){
+    cities.push(new City(225+i*100,600));
+
 }
 
-for (let i = 0; i < 3; i++){
-    cities.push(new City(725 + i * 100, groundY));
+for (let i=0;i<3;i++){
+    cities.push(new City(725+i*100,600));
+
 }
 
-for (let i = 0; i < 3; i++){
-    silos.push(new Silo(112.5 + 487.5 * i, groundY));
+for (let i=0;i<3;i++){
+    silos.push(new Silo(112.5+487.5*i,650));
 }
 
 
-canvas.style.cursor = 'crosshair';
 
-render();
+function gameLoop(){
+    render();
+
+   
+    let RandomMissileSpawn = randInt(0,300);
+    if (RandomMissileSpawn < 3){
+        aMissiles.push(new AttackMissile(randInt(0,1200), 0, cities[randInt(0,cities.length-1)].position.x+25,600));
+
+    }
+    
+    for (i = 0; i < aMissiles.length; i++){
+        if (aMissiles[i].position.y<600){
+            aMissiles[i].move();
+        } else {
+            explosions.push(new Explosion(aMissiles[i].position.x,aMissiles[i].position.y));
+            aMissiles.splice(i,1);
+        }
+        
+    }
+
+    for (i = 0; i < explosions.length; i++){
+        if (explosions[i].radius < 30){
+            explosions[i].radius += 0.25;
+        } 
+        else if (explosions[i].radius < 31) {
+           explosions[i].radius += 0.02;
+        } else {
+            explosions.splice(i,1);
+        }
+    }    
+
+    
+    requestAnimationFrame(gameLoop);
+
+}
+
+gameLoop();
